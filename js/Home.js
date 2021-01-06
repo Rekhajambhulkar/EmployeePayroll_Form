c163744a10880b19649445f1c76f70c30e3f2099
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2021-01-06 13:25:08 +0530

    Ability to Display all employee details from JSON object

--- js/Home.js
+++ js/Home.js
@@ -5,23 +5,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
 /* Template Literal ES6 feature */
 const createInnerHtml = () => {   
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
-const innerHtml = `${headerHtml}
+    let innerHtml = `${headerHtml}`;
+     let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+     innerHtml = `${innerHtml}
    
-<tr>
-    <td><img class="profile" alt="" 
-        src="../assets/e2.jpg">
+    <tr>
+    <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+    <td>${empPayrollData._name}</td>
+    <td>${empPayrollData._gender}</td>
+    <td>${getDeptHtml(empPayrollData._department)}</td> 
+    <td>${empPayrollData._salary}</td>
+    <td>${stringfyDate(empPayrollData._startDate)}</td>
+    <td>
+       <img name="${empPayrollData._id}"  onclick="remove(this)" src="../assets/dlt.jpg" alt="delete">
+       <img name="${empPayrollData._id}"  onclick="update(this)" src="../assets/update1.jpg" alt="edit">  
     </td>
-    <td>Mark Keates</td>
-    <td>Male</td>
-    <td><div class='dept-label'>HR</div>
-        <div class='dept-label'>Finance</div></td>
-        <td>3000000</td>
-        <td>1 Nov 2020</td>
-        <td>
-            <img class="1"  onclick="remove(this)" src="../assets/dlt.jpg" alt="delete">
-            <img class="1"  onclick="update(this)" src="../assets/update1.jpg" alt="edit">  
-        </td>
-</tr>
-`;
+    </tr>
+    `;
+    }
 document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
 }
--- js/new_Employee_PayrollForm.js
+++ js/new_Employee_PayrollForm.js
@@ -1,5 +1,3 @@
-//UC2
-
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
@@ -33,15 +31,17 @@ const save = () => {
     }
 }
 
-function createAndUpdateStorage(employeePayrollData){
+function createAndUpdateStorage(employeePayrollData) {
     let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
     if(employeePayrollList != undefined){
         employeePayrollList.push(employeePayrollData);
-    }else{
-        employeePayrollList = [employeePayrollData];
+    } else{
+        employeePayrollList = [employeePayrollData]
     }
     alert(employeePayrollList.toString());
-    localStorage.setItem("EmployeePayrollList"), JSON.stringify(employeePayrollList)
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
+
 }
 
 const createEmployeePayroll = () => {
@@ -56,58 +56,42 @@ const createEmployeePayroll = () => {
     employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
     employeePayrollData.department = getSelectedValues('[name=department]');
     employeePayrollData.salary = getInputValueById('#salary');
-    employeePayrollDatanote = getInputValueById('#notes');
-    let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+ getInputValueById('#year');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+
+                getInputValueById('#year');
     employeePayrollData.date = Date.parse(date);
     alert(employeePayrollData.toString());
     return employeePayrollData;
 }
 
-const getSelectedValues = (propertyValue => {
+const getSelectedValues = (propertyValue) => {
     let allItems = document.querySelectorAll(propertyValue);
     let selItems = [];
     allItems.forEach(item => {
         if(item.checked) selItems.push(item.value);
     });
     return selItems;
-});
+}
 
+/*
+* 1: querySelector is the newer feature.
+* 2: The querySelector method can be used when selecting by element name,
+*     nesting, or class name.
+* 3: querySelector lets you find elements with rules that can't be
+*     expressed with getElementById
+*/
 const getInputValueById = (id) => {
     let value = document.querySelector(id).value;
     return value;
 }
 
+/*
+* 1: getElementById is better supported than querySelector in older versions
+*     of the browsers.
+* 2: The thing with getElementById is that it only allows to select an
+*     element by its id.
+*/
 const getInputElementValue = (id) => {
     let value = document.getElementById(id).value;
     return value;
-}
-
-const restForm = () => {
-    setValue('#name', '');
-    unsetSelectedValues('[name = profiel]');
-    unsetSelectedValues('[name=gender]');
-    unsetSelectedValues('[name=department]');
-    setValue('#salary', '');
-    setValue('#notes', '');
-    setValue('#day','1');
-    setValue('#month','January');
-    setValue('#year','2020');
-}
-
-const unsetSelectedValues = (propertyValue) => {
-    let allItems = document.querySelectoreAll(propertyValue);
-    allItems.forEach(item => {
-        item.checked = false;
-    });
-}
-
-const setTextValue = (id, value) => {
-    const element = document.querySelector(id);
-    element.textContent = value;
-}
-
-const setValue = (id, value) => {
-    const element = document.querySelector(id);
-    element.value = value;
-
 }
